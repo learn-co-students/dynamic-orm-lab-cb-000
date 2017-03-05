653832e0e86bc89d09a38a65c11d321aeb4aa9a5
github username <github email address>
2017-03-04 22:53:12 -0600

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,50 +4,57 @@ require 'pry'
 
 class InteractiveRecord
 
-	def initialize(options = {}) 
-		options.each do |key, value|
-			self.send("#{key}=", value)
+	def initialize(options = {})
+		options.each do |attr, value|
+			self.send("#{attr}=", value)
 		end
 	end
 
-	def save
-	    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) 
-	    VALUES (#{values_for_insert})"
-	    DB[:conn].execute(sql)
-	    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-  	end
-
-	def self.table_name 
+	def self.table_name
 		self.to_s.downcase.pluralize
 	end
 
-	def self.column_names 
-		DB[:conn].results_as_hash = true #=>NOT .results_by_hash
+	def self.column_names
+		DB[:conn].results_as_hash = true
 
-		sql = "pragma table_info('#{table_name}')"
+		sql = "pragma table_info(#{table_name})"
+		table_info =  DB[:conn].execute(sql)
 
-		table_info = DB[:conn].execute(sql)
 		column_names = []
+
 		table_info.each do |row|
 			column_names << row["name"]
 		end
 		column_names.compact
 	end
 
-	def table_name_for_insert 
+	def table_name_for_insert
 		self.class.table_name
 	end
 
-	def col_names_for_insert 
+	def col_names_for_insert
 		self.class.column_names.delete_if {|col| col == "id"}.join(", ")
 	end
 
-	def values_for_insert 
+	def values_for_insert
 		values = []
+
 		self.class.column_names.each do |col_name|
 			values << "'#{send(col_name)}'" unless send(col_name).nil?
+			# ["'Andrew'", "'10'"]
 		end
-		values.join(", ")
+		values.join(", ") #=> "Andrew", "10"
+	end
+
+	def save
+		sql = <<-SQL
+		INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+		VALUES (#{values_for_insert})
+		SQL
+
+		DB[:conn].execute(sql)
+
+		@id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
 	end
 
 	def self.find_by(attribute)
@@ -59,5 +66,12 @@ class InteractiveRecord
 		sql = "SELECT * FROM #{table_name} WHERE name = '#{name}'" 
 		DB[:conn].execute(sql)
 	end
+
+
+
   
 end
+
+# INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})
+
+#INSERT INTO interactiverecords (name, grade) VALUES ("Andrew", 10)
