f75536d722232cfe46f32da1c503075a602bbb26
Lizzie Sorin <lizzie.sorin@hotmail.fr>
2017-04-19 17:41:26 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,11 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def initialize (option={})
+    otions.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
 end
