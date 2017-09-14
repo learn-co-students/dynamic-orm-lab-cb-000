6b4c024e02cd2911ebfb3cf57556f4d9ea6a85df
williamhancharek <william.hancharek@gmail.com>
2017-09-13 17:31:47 -0700

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,54 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by(name)
+    arr = name.to_a.flatten
+    binding.pry
+    sql = "SELECT * FROM #{self.table_name} WHERE #{arr[0]} = '#{arr[1]}'"
+    DB[:conn].execute(sql)
+  end
+
+  def self.table_name
+    return self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    blah = DB[:conn].execute("PRAGMA table_info(#{self.table_name})")
+    column_names = blah.map {|hash| hash["name"]}
+  end
+
 end
