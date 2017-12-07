6cf1bc4848f375770bd6279fcf250032a8bacf9d
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-07 21:18:57 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,14 +20,14 @@ class InteractiveRecord
       column_names.compact
     end
 
-    def initialize(options={})
+  def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
 
   def save
-    sql = "INSERT INTO #{table_name_for_insert} {#{col_names_for_insert}} VALUES {#{values_for_insert}}"
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
@@ -50,6 +50,13 @@ class InteractiveRecord
 
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by(attribute_hash)
+    value = attribute_hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
     DB[:conn].execute(sql)
   end
 
