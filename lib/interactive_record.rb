8c1829d94aba1f55c653040622db662a65119f74
Michael Anderson <michanders@msn.com>
2017-12-07 21:20:40 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,63 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    column_names = []
+    column = DB[:conn].execute("PRAGMA table_info('#{table_name}')")
+    column.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+
+
+  def initialize(options={})
+    options.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |name|
+      values << "'#{send(name)}'" unless send(name).nil?
+    end
+    values.join(", ")
+  end
+
+  def save
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = '#{name}'")
+  end
+
+  def self.find_by(hash)
+    k = nil
+    v = nil
+    student = hash.each do |key, value|
+      k = key.to_s
+      v = value
+    end
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{k} = '#{v}'")
+  end
+
+
 end
