1a2087256b09afa11ea2ff282cb9a5c6e30639d4
Jen Poole <contact.vandepool@gmail.com>
2018-02-19 21:56:00 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,12 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
+# inherits from InteractiveRecord class
 class Student < InteractiveRecord
+
+  # creates attr_accessors for each column name
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
 
 end
