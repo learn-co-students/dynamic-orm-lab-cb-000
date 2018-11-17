005fc85d688daa1dd6a7f4b05474036d1b11b9ad
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-17 09:15:34 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,5 +52,10 @@ class InteractiveRecord
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
+  
+    def self.find_by(attribute)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.first} = '#{attribute.values.first}'"
+    DB[:conn].execute(sql)
+  end
 
 end
